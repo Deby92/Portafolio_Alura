25c81a41339721a79efa0e57549c295078ced849
Debora Cantuarias <d.cantuarias.urrutia@gmail.com>
2024-11-02 00:25:03 -0300

    sweet alert agregada correctamente

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailField = document.getElementById("email");
+    const errorMessage = document.getElementById("error-message");
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailField.value) {
+        errorMessage.textContent = "El campo de correo electrónico no puede estar vacío.";
+        errorMessage.style.display = "block";
+        return;
+    } else if (!emailPattern.test(emailField.value)) {
+        errorMessage.textContent = "Ingrese un correo electrónico válido en el formato: texto@texto.com.";
+        errorMessage.style.display = "block";
+        return;
+    }
+
+    errorMessage.style.display = "none";
+    Swal.fire({
+        icon: "success",
+        title: "La información se ha enviado correctamente",
+        showConfirmButton: false,
+        timer: 1500
+      });
+});
